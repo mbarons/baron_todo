63d8f322b2926b10c1ff3e4911592c2c5040809d
mbaron <57764271+mbarons@users.noreply.github.com>
2023-10-31 21:40:05 -0300

    post user working

--- Baron-Todo-List-backend/src/main/java/com/baron/Baron/TodoList/config/ProjectSecurityConfig.java
+++ Baron-Todo-List-backend/src/main/java/com/baron/Baron/TodoList/config/ProjectSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.baron.Baron.TodoList.config;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -10,8 +11,11 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 import javax.sql.DataSource;
+import java.util.Collections;
 
 @Configuration
 public class ProjectSecurityConfig {
@@ -19,7 +23,19 @@ public class ProjectSecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.configurationSource(new CorsConfigurationSource() {
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                        CorsConfiguration config = new CorsConfiguration();
+                        config.setAllowedOrigins(Collections.singletonList("https://localhost:4200"));
+                        config.setAllowedMethods(Collections.singletonList("*"));
+                        config.setAllowCredentials(true);
+                        config.setAllowedHeaders(Collections.singletonList("*"));
+                        config.setMaxAge(3600L);
+                        return config;
+                    }
+                }))
+                .csrf(csrf -> csrf.ignoringRequestMatchers("/users/register"))
                 .authorizeHttpRequests((requests) -> requests
                 .requestMatchers("/users/register").permitAll()
                                 .requestMatchers("/todos/{username}").authenticated())
